9b64f4a47282becaf0dfdc7f953e84b0a59a7985
whitestacy8 <whitestacy8@gmail.com>
2016-07-18 18:00:12 -0400

    Update bp_sw_tip_calculator.rb

--- bp_sw_tip_calculator.rb
+++ bp_sw_tip_calculator.rb
@@ -1,12 +1,12 @@
-bill_amount = 100
+bill_amount = gets.chomp
 
 def tip_amount(bill_amount)
-  tip = 0.20*bill_amount
+  tip = 0.20*bill_amount.to_f
   return tip
 end
 
 def total_with_grat(bill_amount,tip_amount)
-  return bill_amount + tip_amount(bill_amount)
+  return bill_amount.to_f + tip_amount(bill_amount).to_f
 end
 
 
